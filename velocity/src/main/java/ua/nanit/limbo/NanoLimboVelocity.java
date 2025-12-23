a9b382ac3233acb5d1ed41ed281a0d369deb3e69
H. Boyd <8950185+hboyd2003@users.noreply.github.com>
2025-12-22 22:24:00 -0500

    fix: correct version in plugin declarations

--- velocity/src/main/java/ua/nanit/limbo/NanoLimboVelocity.java
+++ velocity/src/main/java/ua/nanit/limbo/NanoLimboVelocity.java
@@ -20,7 +20,10 @@ import ua.nanit.limbo.server.Command;
 import ua.nanit.limbo.server.CommandHandler;
 import ua.nanit.limbo.server.LimboServer;
 
-@Plugin(id = "nanolimbovelocity", name = "NanoLimboVelocity", version = "1.0.15", authors = "bivashy, Nan1t")
+@Plugin(id = "nanolimbovelocity",
+        name = "NanoLimboVelocity",
+        version = "1.0.16",
+        authors = "bivashy, Nan1t")
 public class NanoLimboVelocity {
     static {
         NanoLimbo.class.getName(); // For preventing shadow jar minimizing
