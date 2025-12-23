55a3834068b4bf6e4ca6e8e6849388272ee2974a
H. Boyd <8950185+hboyd2003@users.noreply.github.com>
2025-12-22 22:45:06 -0500

    build: add maven local publication

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -7,7 +7,23 @@ java {
     withJavadocJar()
 }
 
+tasks.jar {
+    enabled = false
+}
+
 publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = rootProject.group.toString()
+            artifactId = rootProject.name + "-" + project.name
+            version = rootProject.version.toString()
+
+            artifact(tasks["javadocJar"])
+            artifact(tasks["sourcesJar"])
+            from(components["shadow"])
+        }
+    }
+
     repositories {
         maven {
             mavenLocal()
